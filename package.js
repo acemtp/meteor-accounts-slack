687cbf36050abd3cdbb8088e0a2c59cc92821a52
Vianney <vianney@lempire.co>
2021-09-01 11:29:51 +0200

    meteor 2.3.5

--- package.js
+++ package.js
@@ -1,21 +1,19 @@
 Package.describe({
-  summary: 'Login service for Slack accounts',
-  version: '1.2.0',
-  git: 'https://github.com/efounders/meteor-accounts-slack.git',
   name: 'acemtp:accounts-slack',
+  version: '1.2.0',
+  summary: 'Login service for Slack accounts',
+  git: 'https://github.com/acemtp/meteor-accounts-slack.git',
+  documentation: 'README.md',
 });
 
 Package.onUse(api => {
-  api.versionsFrom('METEOR@2.3.5');
-  api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('http', ['server']);
-  api.use('templating', 'client');
-  api.use('underscore', 'client');
-  api.use('random', 'client');
-  api.use('service-configuration', ['client', 'server']);
-  api.use('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
+  api.versionsFrom('2.3.5');
+
+  api.use(['oauth', 'oauth2', 'service-configuration', 'accounts-base', 'accounts-oauth'], ['client', 'server']);
+
+  api.use(['http@1.4.4'], 'server');
+
+  api.use(['blaze-html-templates@1.0.4', 'underscore', 'random'], 'client');
 
   api.addFiles('slack_server.js', 'server');
 
